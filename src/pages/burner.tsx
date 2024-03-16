3f6ad6d7a6e093806cc71107e1e17da3e7294cb9
tanny <94848716+ilovespectra@users.noreply.github.com>
2024-03-16 13:22:12 -0600

    useRouter lol

--- src/pages/burner.tsx
+++ src/pages/burner.tsx
@@ -1,16 +1,21 @@
 import { NextPage } from "next";
+import { useRouter } from 'next/router';
 import BurnPawnToken from "components/QueensGambit";
 
-const TransferPage: NextPage = () => {
+const BurnPage: NextPage = () => {
+  const router = useRouter();
+  const { firebaseApp } = router.query;
+
   return (
     <div className="container mx-auto">
       <div className="mt-8">
         <div className="flex justify-center mt-4">
-          <BurnPawnToken />
+          {/* Pass the firebaseApp prop to the BurnPawnToken component */}
+          <BurnPawnToken firebaseApp={firebaseApp} />
         </div>
       </div>
     </div>
   );
 };
 
-export default TransferPage;
+export default BurnPage;
