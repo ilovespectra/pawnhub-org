9e2ecc0d0829bc3fdffbb685d5ff5580e6e1f998
tanny <94848716+ilovespectra@users.noreply.github.com>
2024-03-17 12:03:39 -0600

    remove firebase for testing

--- src/pages/burner.tsx
+++ src/pages/burner.tsx
@@ -5,25 +5,12 @@ import { NextPage } from "next";
 import BurnTokenComponent from 'components/QueensGambit';
 import { initializeApp } from 'firebase/app';
 
-const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECTID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
-    appId: process.env.NEXT_PUBLIC_APP_ID
-  };
-  
-  // Initialize Firebase app
-  const app = initializeApp(firebaseConfig, 'my-custom-name');
-
 const BurnPage: NextPage = () => {
   return (
     <div className="container mx-auto">
       <div className="mt-8">
         <div className="flex justify-center mt-4">
-          {/* Pass firebaseApp to BurnTokenComponent */}
-          <BurnTokenComponent firebaseApp={app} />
+          <BurnTokenComponent/>
         </div>
       </div>
     </div>
